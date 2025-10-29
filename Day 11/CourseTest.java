d3a884e3dccf8882f2f865d191825ab55b6195d6
Ajeena Asharaf <ajeenaasharaf636@gmai.com>
2025-10-29 17:32:28 +0530

    change

---
+++ Day 11/CourseTest.java
@@ -0,0 +1,16 @@
+package Day 11;
+
+class Course{
+    course(){
+        String Name = name;
+        int Duration = duration;
+        int No_of_enrollment = no;
+        final String Course_creator = "CodeMentor Academy";
+    }
+    
+}
+public class CourseTest {
+    public static void main(String[] args) {
+        
+    }
+}
